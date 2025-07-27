440665679d2b675718beb85d02a89658c10626ef
crieco <crieco8835@gmail.com>
2025-07-27 10:23:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/shareanote';
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
