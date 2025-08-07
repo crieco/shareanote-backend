da94a1fea8523d5d4e2df52e20328e0a813cc1e2
crieco <crieco8835@gmail.com>
2025-08-06 21:55:55 -0500

    Update threatWords.js

--- threatWords.js
+++ threatWords.js
@@ -1,5 +1,8 @@
 module.exports = [
   'kill', 'murder', 'explode', 'bomb', 'attack', 'terrorist', 'shoot', 'stab',
   'massacre', 'assassinate', 'bloodbath', 'blow up', 'i will kill', 'i will hurt',
-  'threaten', 'school shooting', 'gun', 'knife'
+  'threaten', 'school shooting', 'gun', 'knife', 'die', 'hang myself', 'commit suicide',
+  'i will destroy', 'slaughter', 'shoot up', 'set fire', 'arson', 'behead', 'decapitate',
+  'execute', 'i will bomb', 'i have a gun', 'i have a knife', 'rape', 'sexual assault',
+  'i’m going to hurt', 'plan an attack', 'take lives'
 ];
